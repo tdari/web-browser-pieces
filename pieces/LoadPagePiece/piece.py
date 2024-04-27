8a52755a781b196d18bdf7ac16551653389a8e91
tdari <tunahan.darii@gmail.com>
2024-04-28 00:52:22 +0300

    fix indentation error

--- pieces/LoadPagePiece/piece.py
+++ pieces/LoadPagePiece/piece.py
@@ -8,9 +8,9 @@ class LoadPagePiece(BasePiece):
     def piece_function(self, input_data: InputModel):
         # a better way?
         script = f"""
-        {input_data.script}
-        self.logger.info(f"Loading URL: {input_data.url}")
-        driver.get({input_data.url})
+{input_data.script}
+self.logger.info(f"Loading URL: {input_data.url}")
+driver.get({input_data.url})
 
         """
         return OutputModel(script=script)
--- pieces/OpenBrowserPiece/piece.py
+++ pieces/OpenBrowserPiece/piece.py
@@ -6,20 +6,20 @@ from .models import InputModel, OutputModel
 class OpenBrowserPiece(BasePiece):
     def piece_function(self, input_data: InputModel):
         """
-        from selenium import webdriver
+from selenium import webdriver
 
-        self.logger.info("Execution started")
+self.logger.info("Execution started")
 
-        options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 
-        # try:
-        driver = webdriver.Remote(
-            command_executor="http://localhost:4444/wd/hub"
-        )
+# try:
+driver = webdriver.Remote(
+    command_executor="http://localhost:4444/wd/hub"
+)
 
-        self.logger.info("Maximizing window")
+self.logger.info("Maximizing window")
 
-        driver.maximize_window()
+driver.maximize_window()
         
         """
 
