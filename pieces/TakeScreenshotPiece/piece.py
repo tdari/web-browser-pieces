2d5e6a1b3d3ab99e65d644a64021401007b02b59
tdari <tunahan.darii@gmail.com>
2024-05-05 14:55:26 +0300

    update dynamic path to static path

--- pieces/TakeScreenshotPiece/piece.py
+++ pieces/TakeScreenshotPiece/piece.py
@@ -1,11 +1,8 @@
-import uuid
 from string import Template
 
 from domino.base_piece import BasePiece
 
 from .models import InputModel, OutputModel
-
-image_path = f"/screenshots/{str(uuid.uuid4())}.png"
 
 template = Template(
 """
@@ -20,10 +17,16 @@ driver.save_screenshot("$image_path")
 class TakeScreenshotPiece(BasePiece):
 
     def piece_function(self, input_data: InputModel):
+
+        image_path = f"/home/shared_storage/screenshot.png"
+
         script = template.substitute(
             {
                 "script": input_data.script,
                 "image_path": image_path,
             }
         )
+
+        self.display_result = {"file_type": "png", "file_path": image_path}
+
         return OutputModel(script=script, image_path=image_path)
--- pieces/TakeScreenshotPiece/test_take_screenshot_piece.py
+++ pieces/TakeScreenshotPiece/test_take_screenshot_piece.py
@@ -15,13 +15,15 @@ def test_take_screenshot_piece():
     }
 
     piece_output = piece_dry_run(
-        piece_name="TakeScreenshotPiece", input_data=input_data
+        piece_name="TakeScreenshotPiece",
+        input_data=input_data,
     )
 
     piece_module = importlib.import_module("TakeScreenshotPiece.piece")
+    image_path = "/home/shared_storage/screenshot.png"
 
     assert piece_output["script"] == piece_module.template.substitute(
-        {"script": input_data["script"], "image_path": piece_output["image_path"]}
+        {"script": input_data["script"], "image_path": image_path}
     )
 
     assert piece_output["image_path"].endswith(".png")
